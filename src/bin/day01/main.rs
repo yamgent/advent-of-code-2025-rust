2f677f7c78d8a85af0d92878703fd4f70ce7ca8c
Maurice Wangleng Tan <tanwangleng@outlook.com>
2025-12-01 18:58:49 +0800

    Solve day 1 part 2

--- src/bin/day01/main.rs
+++ src/bin/day01/main.rs
@@ -30,20 +30,20 @@ fn parse_input(input: &str) -> Vec<Instruction> {
     input.trim().lines().map(Instruction::parse).collect()
 }
 
+const START_NUMBER: i32 = 50;
+const TOTAL_NUMBERS: i32 = 100;
+
 fn p1(input: &str) -> String {
     parse_input(input)
         .into_iter()
-        .fold((50i32, 0u32), |mut acc, line| {
-            match line {
-                Instruction::Left(amount) => {
-                    acc.0 -= amount as i32;
-                }
-                Instruction::Right(amount) => {
-                    acc.0 += amount as i32;
-                }
-            }
+        .fold((START_NUMBER, 0u32), |mut acc, line| {
+            let amount = match line {
+                Instruction::Left(amount) => -(amount as i32),
+                Instruction::Right(amount) => amount as i32,
+            };
 
-            acc.0 %= 100;
+            acc.0 = (acc.0 + amount).rem_euclid(TOTAL_NUMBERS);
+
             if acc.0 == 0 {
                 acc.1 += 1;
             }
@@ -55,8 +55,32 @@ fn p1(input: &str) -> String {
 }
 
 fn p2(input: &str) -> String {
-    let _input = input.trim();
-    "".to_string()
+    parse_input(input)
+        .into_iter()
+        .fold((START_NUMBER, 0u32), |mut acc, line| {
+            match line {
+                Instruction::Left(amount) => {
+                    let amount = amount as i32;
+                    if acc.0 == amount {
+                        acc.1 += 1;
+                    } else if acc.0 < amount {
+                        let remaining = amount - acc.0;
+                        acc.1 +=
+                            if acc.0 == 0 { 0 } else { 1 } + (remaining / TOTAL_NUMBERS) as u32;
+                    }
+                    acc.0 = (acc.0 - amount).rem_euclid(TOTAL_NUMBERS);
+                }
+                Instruction::Right(amount) => {
+                    let amount = amount as i32;
+                    acc.1 += ((acc.0 + amount) / TOTAL_NUMBERS) as u32;
+                    acc.0 = (acc.0 + amount).rem_euclid(TOTAL_NUMBERS);
+                }
+            }
+
+            acc
+        })
+        .1
+        .to_string()
 }
 
 fn main() {
@@ -106,12 +130,40 @@ L82
 
     #[test]
     fn test_p2_sample() {
-        assert_eq!(p2(SAMPLE_INPUT), "");
+        assert_eq!(p2(SAMPLE_INPUT), "6");
     }
 
     #[test]
-    #[ignore = "not yet implemented"]
+    fn test_p2_additional() {
+        [
+            ("L50", 1),
+            ("L50\nL99", 1),
+            ("L50\nL100", 2),
+            ("L50\nL101", 2),
+            ("L50\nL199", 2),
+            ("L50\nL200", 3),
+            ("L50\nL201", 3),
+            ("L50\nR99", 1),
+            ("L50\nR100", 2),
+            ("L50\nR101", 2),
+            ("L50\nR199", 2),
+            ("L50\nR200", 3),
+            ("L50\nR201", 3),
+        ]
+        .into_iter()
+        .for_each(|(input, expected)| {
+            assert_eq!(
+                p2(input),
+                expected.to_string(),
+                "{}, {}",
+                input.replace("\n", ";"),
+                expected
+            );
+        });
+    }
+
+    #[test]
     fn test_p2_actual() {
-        assert_eq!(p2(ACTUAL_INPUT), "");
+        assert_eq!(p2(ACTUAL_INPUT), "6223");
     }
 }
