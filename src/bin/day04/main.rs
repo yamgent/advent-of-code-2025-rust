6466727970bea949f62cfa661bc38c3021ad0d37
Maurice Wangleng Tan <tanwangleng@outlook.com>
2025-12-04 19:16:52 +0800

    Solve day 4 part 2

--- src/bin/day04/main.rs
+++ src/bin/day04/main.rs
@@ -52,8 +52,68 @@ fn p1(input: &str) -> String {
 }
 
 fn p2(input: &str) -> String {
-    let _input = input.trim();
-    "".to_string()
+    let mut grid: HashSet<_> =
+        HashSet::from_iter(input.trim().lines().enumerate().flat_map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .filter(|(_, ch)| *ch == '@')
+                .map(move |(x, _)| (x as i64, y as i64))
+        }));
+
+    let size = (
+        input
+            .trim()
+            .lines()
+            .next()
+            .expect("at least one row")
+            .chars()
+            .count(),
+        input.trim().lines().count(),
+    );
+
+    let mut iterations = 0;
+    let mut count = 0;
+    let mut last_coords = vec![];
+
+    while iterations == 0 || last_coords.len() > 0 {
+        // not the most efficient but at least it does the job now
+        last_coords = (0_i64..size.0 as i64)
+            .flat_map(|x| {
+                (0_i64..size.1 as i64)
+                    .filter(|y| grid.contains(&(x, *y)))
+                    .filter(|y| {
+                        let coord = (x as i64, *y);
+
+                        [
+                            (-1, -1),
+                            (-1, 0),
+                            (-1, 1),
+                            (0, -1),
+                            (0, 1),
+                            (1, -1),
+                            (1, 0),
+                            (1, 1),
+                        ]
+                        .into_iter()
+                        .map(|relative| (coord.0 + relative.0, coord.1 + relative.1))
+                        .filter(|adjacent| grid.contains(adjacent))
+                        .count()
+                            < 4
+                    })
+                    .map(|y| (x, y))
+                    .collect::<Vec<_>>()
+            })
+            .collect();
+
+        iterations += 1;
+        count += last_coords.len();
+
+        last_coords.iter().for_each(|coord| {
+            grid.remove(&coord);
+        });
+    }
+
+    count.to_string()
 }
 
 fn main() {
@@ -90,12 +150,11 @@ mod tests {
 
     #[test]
     fn test_p2_sample() {
-        assert_eq!(p2(SAMPLE_INPUT), "");
+        assert_eq!(p2(SAMPLE_INPUT), "43");
     }
 
     #[test]
-    #[ignore = "not yet implemented"]
     fn test_p2_actual() {
-        assert_eq!(p2(ACTUAL_INPUT), "");
+        assert_eq!(p2(ACTUAL_INPUT), "8910");
     }
 }
